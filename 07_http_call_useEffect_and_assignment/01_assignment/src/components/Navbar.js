56aebc0f81d720d08800b6555b1da153e061cdd3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-15 13:00:13 +0530

    dir name change

--- 07_http_call_useEffect_and_assignment/01_assignment/src/components/Navbar.js
+++ 07_http_call_useEffect_and_assignment/01_assignment/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-dark ">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Navbar
