7fc3f1458d97257479b05a49ecaf1be1c8b8be65
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-20 17:45:31 +0530

    update product

--- 11_formik_&_yup/src/components/Editproduct.js
+++ 11_formik_&_yup/src/components/Editproduct.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getProductById } from "../services/Product_Service";
+import { useNavigate, useParams } from "react-router-dom";
+import { getProductById, updateProduct } from "../services/Product_Service";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
 const proSchema = Yup.object().shape({
   pname: Yup.string().required("Pname is required"),
   price: Yup.string().required("Price is required"),
-  quantity: Yup.string().required("Quantity is required"),
+  quantity: Yup.string().required("Quanitity is required"),
 });
-
 export default function Editproduct() {
-  let [state, setState] = useState({});
+  let [state, setState] = useState(null);
+  let navigate = useNavigate();
   let { id } = useParams();
 
   useEffect(() => {
@@ -21,30 +20,54 @@ export default function Editproduct() {
     });
   }, []);
 
+  const postData = (data) => {
+    updateProduct(id, data).then((res) => {
+      alert("Product Updated");
+      navigate("/products");
+    });
+  };
+
   return (
     <div>
       <h2> Edit Product</h2>
-      <Formik
-        initialValues={{ pname: "", price: "", quantity: "" }}
-        validationSchema={proSchema}
-        onSubmit={(values) => console.log(values)}
-      >
-        <Form>
-          <div className="form-group">
-            <label> Pname</label>
-            <Field type="text" className="form-control" name="pname" />
-          </div>
-          <div className="form-group">
-            <label> Price</label>
-            <Field type="text" className="form-control" name="price" />
-          </div>
-          <div className="form-group">
-            <label> Quantity</label>
-            <Field type="text" className="form-control" name="quantity" />
-          </div>
-          <input type="submit" value="Add" className="btn btn-success" />
-        </Form>
-      </Formik>
+      {state != null && (
+        <Formik
+          initialValues={state}
+          validationSchema={proSchema}
+          onSubmit={(values) => postData(values)}
+        >
+          <Form>
+            <div className="form-group">
+              <label> Pname</label>
+              <Field type="text" className="form-control" name="pname" />
+              <ErrorMessage
+                name="pname"
+                component="div"
+                className="alert alert-danger"
+              />
+            </div>
+            <div className="form-group">
+              <label> Price</label>
+              <Field type="text" className="form-control" name="price" />
+              <ErrorMessage
+                name="price"
+                component="div"
+                className="alert alert-danger"
+              />
+            </div>
+            <div className="form-group">
+              <label> Quantity</label>
+              <Field type="text" className="form-control" name="quantity" />
+              <ErrorMessage
+                name="quantity"
+                component="div"
+                className="alert alert-danger"
+              />
+            </div>
+            <input type="submit" value="Add" className="btn btn-success" />
+          </Form>
+        </Formik>
+      )}
     </div>
   );
 }
