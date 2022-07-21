c952ff4bdb7cb8f9d39cedafd8fe4d9d49f602ce
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-21 16:11:46 +0530

    profile picture

--- 12_employees_crud_assignment/src/components/Employees.js
+++ 12_employees_crud_assignment/src/components/Employees.js
@@ -44,9 +44,19 @@ function Employees() {
                 >
                   <div className="card ">
                     <div className="card-body">
-                      <h5 className="card-title">
-                        Employee : {employee.employee_name}
-                      </h5>
+                      <div className="d-flex gap-2 align-items-center mb-2">
+                        <span class="d-inline-block w-25 h-25 rounded-circle overflow-hidden bg-light">
+                          <svg
+                            class="h-100 w-100 text-secondary"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                          </svg>
+                        </span>
+
+                        <h5 className="card-title">{employee.employee_name}</h5>
+                      </div>
+
                       <p className="card-text">
                         <span>Salary: {employee.employee_salary}</span>
                         <br />
