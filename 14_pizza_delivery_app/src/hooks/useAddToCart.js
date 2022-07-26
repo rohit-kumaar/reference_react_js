dd449425a0793bcbb1e4e1b44f631abad77f60fb
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-26 13:39:56 +0530

    alert  msg

--- 14_pizza_delivery_app/src/hooks/useAddToCart.js
+++ 14_pizza_delivery_app/src/hooks/useAddToCart.js
@@ -3,11 +3,11 @@ export const addToCart = (product) => {
     let cartItem = JSON.parse(localStorage.getItem('cart'));
     cartItem.push(product);
     localStorage.setItem('cart', JSON.stringify(cartItem));
-    alert('Meal Is Add To Cart');
+    alert('Your Meal Is Add To Cart');
   } else {
     let cartItem = [];
     cartItem.push(product);
     localStorage.setItem('cart', JSON.stringify(cartItem));
-    alert('Meal Is Add To Cart');
+    alert('Your Meal Is Add To Cart');
   }
 };
