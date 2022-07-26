60edf67555f8b4990174109cb00b996fe1ea99f3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-26 12:36:59 +0530

    toggle

--- 14_pizza_delivery_app/src/components/Header.jsx
+++ 14_pizza_delivery_app/src/components/Header.jsx
@@ -1,27 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import logo from '../assets/images/logo.PNG';
 import Btn from './Btn';
 
 function Header() {
-  const navigate = useNavigate();
-  // const [menu, setMenu] = useState('bx-menu');
-  // const [circle, setCircle] = useState('bx-x-circle');
-
-  const hamburger = () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const active = hamburgerMenu.classList.toggle('active');
-    if (active) {
-      // setMenu('bx-x-circle');
-      hamburger.classList.add('bx-menu');
-      hamburger.classList.remove('bx-x-circle');
-    } else {
-      // setCircle('bx-menu');
-      hamburger.classList.remove('bx-menu');
-      hamburger.classList.add('bx-x-circle');
-    }
-  };
+  const [toggle, setToggle] = useState(false);
 
   return (
     <>
@@ -57,7 +40,11 @@ function Header() {
 
             {window.location.href === 'http://localhost:3000/menu-list' && (
               <nav>
-                <ul className="hamburger-menu active fixed top-0 right-0 bottom-0 left-20 z-10 flex flex-col gap-6 bg-slate-200/40 p-12 backdrop-blur-xl transition-transform duration-700 sm:static sm:left-0 sm:translate-x-0 sm:flex-row sm:items-center sm:justify-center sm:bg-transparent sm:p-0 sm:transition-none">
+                <ul
+                  className={`
+                  ${toggle == false ? 'active' : ''}  
+                   fixed top-0 right-0 bottom-0 left-20 z-10 flex flex-col gap-6 bg-slate-200/40 p-12 backdrop-blur-xl transition-transform duration-700 sm:static sm:left-0 sm:translate-x-0 sm:flex-row sm:items-center sm:justify-center sm:bg-transparent sm:p-0 sm:transition-none`}
+                >
                   <li>
                     <Link to="#" className="font-medium text-slate-500">
                       Menu
@@ -89,8 +76,10 @@ function Header() {
 
                 <div className="cursor-pointer sm:hidden">
                   <i
-                    className={`bx bx-menu  relative z-20 text-4xl`}
-                    onClick={() => hamburger()}
+                    className={`
+                     ${toggle === true ? 'bx-x-circle' : ' bx-menu'}
+                    bx relative z-20 text-4xl`}
+                    onClick={() => setToggle(!toggle)}
                   ></i>
                 </div>
               </nav>
