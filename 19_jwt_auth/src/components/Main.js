25af54f17d46039f1aa4f9025d12410e0711719f
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-08-19 10:26:49 +0530

    prettify jwt auth code

--- 19_jwt_auth/src/components/Main.js
+++ 19_jwt_auth/src/components/Main.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Main() {
-  return (
-    <div>Main</div>
-  )
+  return <div>Main</div>;
 }
--- 19_jwt_auth/src/services/Auth.js
+++ 19_jwt_auth/src/services/Auth.js
@@ -1,30 +1,33 @@
 import axios from "axios";
-import jwt_decode from 'jwt-decode'
-const API_URL="http://localhost:3001/api/v1/";
-function loginUser(data){
-    return axios.post(`${API_URL}auth`,data);
+import jwt_decode from "jwt-decode";
+const API_URL = "http://localhost:3001/api/v1/";
+
+function loginUser(data) {
+  return axios.post(`${API_URL}auth`, data);
 }
-function isLoggedIn(){
-    const data=localStorage.getItem("_token");
-    if(!data){
-        return false;
-    }
-    else {
-        return true;
-    }
+
+function isLoggedIn() {
+  const data = localStorage.getItem("_token");
+  if (!data) {
+    return false;
+  } else {
+    return true;
+  }
 }
-function getToken(){
-    return localStorage.getItem("_token")
+
+function getToken() {
+  return localStorage.getItem("_token");
 }
-function getUser(){
-    try{
+
+function getUser() {
+  try {
     return jwt_decode(localStorage.getItem("_token"));
-    }
-    catch(e){
-        return null;
-    }
+  } catch (e) {
+    return null;
+  }
 }
-function isAdmin(){
-    return !getUser()?false:getUser().isAdmin;
+
+function isAdmin() {
+  return !getUser() ? false : getUser().isAdmin;
 }
-export {isLoggedIn,loginUser,getToken,getUser,isAdmin}
+export { isLoggedIn, loginUser, getToken, getUser, isAdmin };
