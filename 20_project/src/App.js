d0ff45a14e269de3b3b409da3095dfb57359d678
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-08-23 18:11:25 +0530

    header

--- 20_project/src/App.js
+++ 20_project/src/App.js
@@ -1,10 +1,20 @@
-import { Button } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "components/Header";
+import "App.css";
+import Home from "pages/Home";
+import SignUp from "pages/SignUp";
+import Login from "pages/Login";
 
 function App() {
   return (
-    <>
-      <Button variant="contained">Hello World</Button>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
